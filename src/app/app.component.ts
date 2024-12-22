9f0ae7767c47be87729bb7433322703e1a1ad6ab
Ilja-Furer <iljafurer@gmail.com>
2024-12-22 18:02:13 +0100

    added funcitonality to the sections on the page. TODO: Add content to each section; Rework look of website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,68 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  firstName = 'Ilja';
-  lastName = 'Furer';
-  fadeOut = false;
-  moveToTop = false;
+  firstName: string = 'ILJA';
+  lastName: string = 'FURER';
+  moveToTop: boolean = false;
+  fadeOut: boolean = false;
+  showHobbies: boolean = false;  
+  showProjects: boolean = false;  
 
   constructor(private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
     setTimeout(() => {
       this.fadeOut = true;
-
       setTimeout(() => {
         this.moveToTop = true;
-        this.cdr.detectChanges(); // Force update if needed
+        this.cdr.detectChanges();
       }, 500);
     }, 2000);
+  }
+
+  onSlideRightTopClick() {
+    console.log('Hobbies clicked!');
+    this.showHobbies = true;
+    this.showProjects = false;  
+
+    document.documentElement.classList.add('no-scroll');
+    setTimeout(() => {
+      window.scrollTo({
+        top: window.innerHeight,
+        behavior: 'smooth'
+      });
+    }, 100);
+  }
+
+  onSlideRightBottomClick() {
+    console.log('Projects clicked!');
+    this.showProjects = true;
+    this.showHobbies = false;  
+
+    document.documentElement.classList.add('no-scroll');
+    setTimeout(() => {
+      window.scrollTo({
+        top: window.innerHeight,
+        behavior: 'smooth'
+      });
+    }, 100);
+  }
+
+  closeHobbies() {
+    this.showHobbies = false;
+    document.documentElement.classList.remove('no-scroll');
+  }
+
+  closeProjects() {
+    this.showProjects = false;
+    document.documentElement.classList.remove('no-scroll');
   }
 }

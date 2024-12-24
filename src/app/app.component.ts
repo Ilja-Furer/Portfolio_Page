926c7f7ddd9935a20749d3dc4dcc5b2cea4923d8
Ilja-Furer <iljafurer@gmail.com>
2024-12-24 14:39:05 +0100

    add intro animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { Location } from '@angular/common';
 
@@ -8,28 +8,38 @@ import { Location } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterViewInit {
   location = inject(Location);
   router = inject(Router);
+  
+  @ViewChild('overlay')
+  overlay!: ElementRef;
 
-  disabled = this.shouldDisable();
+  disabled = this.isRoot();
+  playAnimation = this.isRoot();
 
   ngOnInit(): void {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        this.disabled = this.shouldDisable();
+        this.disabled = this.isRoot();
       }
     })
+  }
+
+  ngAfterViewInit(): void {
+    this.overlay.nativeElement.addEventListener('animationend', () => {
+      this.playAnimation = false;
+    });
   }
 
   navigateBack(): void {
     if (!this.location.isCurrentPathEqualTo("/")) {
       this.location.back();
-      this.disabled = this.shouldDisable();
+      this.disabled = this.isRoot();
     }
   }
 
-  private shouldDisable(): boolean {
+  private isRoot(): boolean {
     return this.location.isCurrentPathEqualTo("/");
   }
 }
